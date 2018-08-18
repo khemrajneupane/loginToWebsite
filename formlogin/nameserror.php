065a06bf3256e11fd66f0748927e41eac7867416
RajK <shekharneupane@gmail.com>
2018-08-18 12:50:43 +0300

    Update nameserror.php

--- formlogin/nameserror.php
+++ formlogin/nameserror.php
@@ -1,19 +1,17 @@
-
 <?php
-
 	include_once 'signup.php';
-	
 ?>
+
 <!DOCTYPE html>
 <html>
-<head><title>First name last name error!</title>
-<link type = "text/css" rel = "stylesheet" href = "login.css">
-</head>
+	<head><title>First name last name error!</title>
+		<link type = "text/css" rel = "stylesheet" href = "login.css">
+	</head>
 <body>
-<section>
-<div class = "main-rapper">
-<h1>First & last names only accept character alphabets!</h1>
-</div>
-</section>
+	<section>
+		<div class = "main-rapper">
+		<h1>First & last names only accept character alphabets!</h1>
+		</div>
+	</section>
 </body>
 </html>
